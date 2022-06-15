a2ec9c2a73a231799dbdc33bb166b76bf6728abc
daniels <ddan.mm.16@gmail.com>
2022-06-15 17:22:22 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Toaster } from "react-hot-toast";
+import Routes from "./routes";
+
+function App() {
+  return (
+    <div>
+      <Routes />
+      <Toaster />
+    </div>
+  );
+}
+
+export default App;
